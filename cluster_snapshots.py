744696e2cf593254bba432b9e267db34377208a6
OzYossarian <alex.townsend-teague@outlook.com>
2020-07-15 12:53:18 +0100

    Initial commit

---
+++ cluster_snapshots.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def snapshot_distances(tnet, dist='eucledian'):
+    snapshots = tnet.df_to_array()
+    # put time as zeroth axis
+    snapshots = np.swapaxes(snapshots, 0, 2)
+
+    T = tnet.T
+    dist_mat = np.zeros((T, T))
+
+    for i in range(T):
+        for j in range(i):
+            # Eucledian distance
+            dist_mat[j, i] = np.linalg.norm(snapshots[i] - snapshots[j])
+
+            # extract condensed distance matrix need for the clustering
+    # id_l = np.tril_indices(n=T, k=-1) # indices of lower triangle elements
+    id_u = np.triu_indices(n=T, k=1)  # indices of lower triangle elements
+
+    # dist_mat_condensed = dist_mat[id_l]
+    dist_mat_condensed = dist_mat[id_u]
+
+    return dist_mat_condensed
---
+++ graphs_playground.py
@@ -0,0 +1,3 @@
+from xppcall import xpprun
+
+npa, variables = xpprun('bychen04_xpp.ode', clean_after=True)

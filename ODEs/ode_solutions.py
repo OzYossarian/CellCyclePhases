553b2fa180a60daabe92dd0d573b004d4645a104
OzYossarian <alex.townsend-teague@outlook.com>
2020-07-21 08:27:31 +0100

    Mass minima now get plotted correctly (fixed indexing error)

--- ODEs/ode_solutions.py
+++ ODEs/ode_solutions.py
@@ -6,8 +6,8 @@ from ODEs.xppcall import xpprun
 class OdeSolutions:
     def __init__(self, filepath, start_time, end_time):
         times_and_solutions, variables = xpprun(filepath, clean_after=True)
-        self.times = times_and_solutions[start_time:end_time, 0]
-        self.solutions = times_and_solutions[start_time:end_time, 1:]
+        self.times = times_and_solutions[start_time:end_time + 1, 0]
+        self.solutions = times_and_solutions[start_time:end_time + 1, 1:]
         self.variables = variables
         self.start_time = start_time
         self.end_time = end_time

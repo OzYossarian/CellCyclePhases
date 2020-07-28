2e4f6af9fa855badd00008d2df1b60929e711bb2
OzYossarian <alex.townsend-teague@outlook.com>
2020-07-28 12:27:22 +0100

    Similar to previous commit: plotting average silhouette samples against whatever the cluster limit type is - i.e. not just against max no. of clusters. Have noted in brackets the actual number of clusters in a cluster set.
    Added some basic error handling

--- clustering/Silhouettes.py
+++ clustering/Silhouettes.py
@@ -28,5 +28,11 @@ class Silhouettes(Sequence):
 
 class Silhouette:
     def __init__(self, distance_data, clusters, metric):
-        self.average = metrics.silhouette_score(distance_data, clusters, metric=metric)
-        self.samples = metrics.silhouette_samples(distance_data, clusters, metric=metric)
+        try:
+            self.average = metrics.silhouette_score(distance_data, clusters, metric=metric)
+            self.samples = metrics.silhouette_samples(distance_data, clusters, metric=metric)
+        except ValueError as error:
+            # Often the number of clusters is 1, which sklearn does not like.
+            print(f'WARNING: unable to compute silhouette for cluster set. Error is: {error}')
+            self.average = 0
+            self.samples = np.array([])
--- clustering/silhouetting.py
+++ clustering/silhouetting.py
@@ -6,7 +6,7 @@ from clustering import clustering
 from drawing.utils import display_name
 
 
-def plot_average_silhouettes_and_clusters(cluster_sets, cluster_limit_type, times, title):
+def plot_average_silhouettes_and_clusters(cluster_sets, times, title):
     gridspec_kw = {"width_ratios": [3, 1, 2]}
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(9, 3), gridspec_kw=gridspec_kw, sharey=True)
 
@@ -17,14 +17,14 @@ def plot_average_silhouettes_and_clusters(cluster_sets, cluster_limit_type, time
     ax2.yaxis.set_tick_params(labelleft=True)
     ax3.yaxis.set_tick_params(labelleft=True)
 
-    ax1.set_ylabel(display_name(cluster_limit_type))
+    ax1.set_ylabel(display_name(cluster_sets.limit_type))
     fig.suptitle(title)
     plt.subplots_adjust(wspace=0.4, top=0.8)
 
     return fig, (ax1, ax2, ax3)
 
 
-def plot_average_silhouettes(cluster_sets,ax=None):
+def plot_average_silhouettes(cluster_sets, ax=None):
     if ax is None:
         ax = plt.gca()
 
@@ -34,23 +34,15 @@ def plot_average_silhouettes(cluster_sets,ax=None):
 
 
 def plot_silhouettes_samples(cluster_sets, columns):
-    unique_numbers_of_clusters, indices_of_unique_numbers_of_clusters = \
-        np.unique(cluster_sets.sizes, return_index=True)
-
-    # Omit the 1-cluster
-    one_cluster_index = np.where(unique_numbers_of_clusters == 1)
-    unique_numbers_of_clusters = np.delete(unique_numbers_of_clusters, one_cluster_index)
-    indices_of_unique_numbers_of_clusters = np.delete(indices_of_unique_numbers_of_clusters, one_cluster_index)
-    total_subplots = len(unique_numbers_of_clusters)
-
+    total_subplots = len(cluster_sets)
     rows = (total_subplots // columns) + (0 if total_subplots % columns == 0 else 1)
     fig, axs = plt.subplots(nrows=rows, ncols=columns, sharex=True, sharey=True, figsize=(10, 2 * rows))
 
-    for i, unique_j in enumerate(indices_of_unique_numbers_of_clusters):
+    for i, cluster_set in enumerate(cluster_sets):
         ax = axs.flatten()[i]
-        cluster_set = cluster_sets[unique_j]
-        title = f"{int(cluster_set.size)} clusters"
-        plot_silhouette_samples(cluster_set, ax=ax, title=title)
+        title = f'{display_name(cluster_set.limit_type)} = {cluster_set.limit}'
+        subtitle = f'({int(cluster_set.size)} clusters)'
+        plot_silhouette_samples(cluster_set, ax=ax, title=f'{title}\n{subtitle}')
 
     xlabel, ylabel = 'Silhouette score', 'Ordered time points'
     drawing.utils.label_subplot_grid_with_shared_axes(rows, columns, total_subplots, xlabel, ylabel, fig, axs)
@@ -71,17 +63,18 @@ def plot_silhouette_samples(cluster_set, ax=None, title=''):
     for i, cluster in enumerate(np.unique(cluster_set.clusters)):
         # Aggregate the silhouette scores for samples belonging to each cluster, and sort them
         # ToDo: calculate values upon creation of Silhouette(s)? Then store them as silhouette(s).values?
-        silhouette_values = cluster_set.silhouette.samples[cluster_set.clusters == cluster]
-        silhouette_values.sort()
+        if cluster_set.silhouette.samples.size > 0:
+            silhouette_values = cluster_set.silhouette.samples[cluster_set.clusters == cluster]
+            silhouette_values.sort()
 
-        silhouette_size = silhouette_values.shape[0]
-        y_upper = y_lower + silhouette_size
-        y = np.arange(y_lower, y_upper)
-        ax.fill_betweenx(y, 0, silhouette_values, facecolor=f"C{i}", edgecolor=f"C{i}", alpha=1)
+            silhouette_size = silhouette_values.shape[0]
+            y_upper = y_lower + silhouette_size
+            y = np.arange(y_lower, y_upper)
+            ax.fill_betweenx(y, 0, silhouette_values, facecolor=f"C{i}", edgecolor=f"C{i}", alpha=1)
 
-        # Compute the new y_lower for next plot
-        vertical_padding = 1
-        y_lower = y_upper + vertical_padding
+            # Compute the new y_lower for next plot
+            vertical_padding = 1
+            y_lower = y_upper + vertical_padding
 
     ax.set_title(title)
     ax.axvline(x=cluster_set.silhouette.average, c='k', ls='--')

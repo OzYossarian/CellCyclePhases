4108b8199fd554f22085234bfa9dd61d5b7406a4
OzYossarian <alex.townsend-teague@outlook.com>
2020-07-29 08:50:22 +0100

    Generalising short method to compute relative optima

--- ODEs/ODEsSolutions.py
+++ ODEs/ODEsSolutions.py
@@ -1,4 +1,9 @@
+import numpy as np
+import scipy
+
 from ODEs.xppcall import xpprun
+
+comparators = {'minima': np.less, 'maxima': np.greater}
 
 
 class ODEsSolutions:
@@ -7,10 +12,14 @@ class ODEsSolutions:
         self.times = times_and_solutions[start_time:end_time, 0]
         self.solutions = times_and_solutions[start_time:end_time, 1:]
         self.variables = variables
-        self.start_time = start_time  # inclusive
-        self.end_time = end_time  # exclusive
-        # variables = [var.upper() for var in variables]
-        # data = {var: series(var) for var in variables}
+        self.start_time = start_time  # Inclusive
+        self.end_time = end_time  # Exclusive
 
     def series(self, variable):
         return self.solutions[:, self.variables.index(variable)]
+
+    def relative_optima(self, variable, optima_type):
+        series = self.series(variable)
+        optima_times = scipy.signal.argrelextrema(series, comparators[optima_type])
+        optima = series[optima_times]
+        return optima, optima_times

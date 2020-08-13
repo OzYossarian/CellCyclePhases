5c006c27adfa135daaa5eea821f3a119fd3e28b8
OzYossarian <alex.townsend-teague@outlook.com>
2020-08-13 18:33:26 +0100

    Adding notebook for creating a temporal network from the circadian data.
    Added a new constructor for TemporalNetwork and edited some older ones.

--- networks/TemporalNetwork.py
+++ networks/TemporalNetwork.py
@@ -4,8 +4,6 @@ import numpy as np
 import pandas as pd
 import teneto
 
-from networks import static_networks
-
 
 class TemporalNetwork:
     def __init__(self, teneto_network, time_shift=0, node_ids_to_names=None):
@@ -38,19 +36,19 @@ class TemporalNetwork:
             return self.node_ids_to_names[id]
 
     @classmethod
-    def from_snapshots(_class, snapshots, node_ids_to_names=None):
+    def from_snapshots_numpy_array(_class, snapshots, node_ids_to_names=None):
         # 'snapshots' should be a numpy.array with dimensions (time, nodes, nodes)
         array = np.swapaxes(snapshots, 0, 2)
-        return TemporalNetwork.from_array(array, node_ids_to_names)
+        return TemporalNetwork.from_numpy_array(array, node_ids_to_names)
 
     @classmethod
-    def from_array(_class, array, node_ids_to_names=None):
+    def from_numpy_array(_class, array, node_ids_to_names=None):
         # 'array' should be a numpy.array with dimensions (nodes, nodes, time)
         teneto_network = teneto.TemporalNetwork(from_array=array)
         return _class(teneto_network, node_ids_to_names=node_ids_to_names)
 
     @classmethod
-    def from_edge_list_dataframe(_class, edges):
+    def from_edge_list_dataframe(_class, edges, normalise=False, threshold=0, binary=False):
         # 'edges' should be a pandas.DataFrame
         number_of_columns = edges.shape[1]
         # Columns must be named i, j and t, with optional weight column
@@ -62,6 +60,12 @@ class TemporalNetwork:
             raise ValueError('List of edges requires either 3 or 4 columns')
         edges.columns = columns
 
+        if normalise:
+            edges['weight'] = (edges['weight'] / edges['weight'].max())
+        edges = edges[edges['weight'] >= threshold]
+        if binary:
+            edges[edges['weight'] > 0]['weight'] = 1
+
         # Replace node names with numeric values
         nodes = sorted(set(edges[['i', 'j']].values.flatten()))
         names_to_ids = {key: i for i, key in enumerate(nodes)}
@@ -69,6 +73,7 @@ class TemporalNetwork:
         edges = edges.replace(names_to_ids)
 
         edges = edges.sort_values('t')
+        edges.reset_index(drop=True, inplace=True)
         start_time = edges['t'][0]
         if start_time != 0:
             # For compatibility with teneto, shift all times so that we start at time 0
@@ -77,65 +82,119 @@ class TemporalNetwork:
         return _class(teneto.TemporalNetwork(from_df=edges), start_time, ids_to_names)
 
     @classmethod
-    def from_edge_list_file(_class, filepath, separator=None):
-        edges = pd.read_csv(filepath, sep=separator, engine='python')
-        return TemporalNetwork.from_edge_list_dataframe(edges)
-
-    @classmethod
-    def from_snapshots_file(_class, snapshots_filepath, node_ids_to_names_filepath=None):
-        snapshots = load_file(snapshots_filepath)
-        node_ids_to_names = load_file(node_ids_to_names_filepath)
-        if isinstance(node_ids_to_names, np.ndarray):
-            # Extract the dictionary from the numpy array
-            node_ids_to_names = node_ids_to_names.item()
-        return _class.from_snapshots(snapshots, node_ids_to_names)
-
-    @classmethod
-    def from_static_network(
+    def from_static_network_and_edge_list_dataframe(
             _class,
             static_network,
-            temporal_edge_data_filepath=None,
-            temporal_node_data_filepath=None,
-            temporal_data_separator=None,
+            edges,
+            normalise=False,
             threshold=0,
-            combine_node_weights=lambda x, y: x * y,
-            binary=False,
-            normalise=False):
+            binary=False):
 
-        # 'static_network' must be an instance of networkx.Graph.
-        # Temporal data file should be as described in the function edge_list_from_temporal_node_data or
-        # edge_list_from_temporal_edge_data.
-        # If 'normalise' is True, all weights will be divided through by the max weight.
-        # Only edges with weight at least 'threshold' (AFTER normalising but BEFORE binarying) will be kept.
-        # If 'binary' is True, all edges with positive weight will be reassigned weight 1.
+        # 'static_network' should be a networkx.Graph.
 
-        if temporal_node_data_filepath:
-            edges = static_networks.edge_list_from_temporal_node_data(
-                static_network, temporal_node_data_filepath, temporal_data_separator, combine_node_weights)
-        elif temporal_edge_data_filepath:
-            edges = static_networks.edge_list_from_temporal_edge_data(
-                static_network, temporal_edge_data_filepath, temporal_data_separator)
-        else:
-            raise ValueError('Provide exactly one of temporal edge data or temporal node data')
+        # 'edges' should be a pandas.DataFrame with with three or four columns, representing source node, target
+        # node, time and (optionally) weight, respectively. If the weight column is omitted then all weights are
+        # taken to be 1.
 
-        if normalise:
-            max_weight = edges['w'].max()
-            edges['w'] = (edges['w'] / max_weight)
-        edges = edges[edges['w'] >= threshold]
-        if binary:
+        # If 'normalise' is True, all weights will be divided through by the max weight across all edges.
+
+        # For nodes i and j, if the static network contains an edge ij, and at time t the edge ij has weight w at
+        # least 'threshold' (after normalisation), then the temporal network will contain an edge ij at time t of
+        # weight w.
+
+        # If 'binary' is True, then any positive edges that exceed the threshold will be set to 1.
+
+        if len(edges.columns) == 3:
             edges['w'] = 1
+        elif len(edges.columns) != 4:
+            raise ValueError('Edge list should have either 3 or 4 columns.')
+        edges.columns = ['i', 'j', 't', 'w']
 
-        edges.reset_index(drop=True, inplace=True)
-        return _class.from_edge_list_dataframe(edges)
+        # ToDo: applying this lambda is VERY slow - perhaps instead convert static_network.edges into a pandas
+        # dataframe and join on it? Should be faster.
+        missing_edges_indices = edges.apply(lambda edge: not static_network.has_edge(edge['i'], edge['j']), axis=1)
+        missing_edges = edges[missing_edges_indices]
+        if not missing_edges.empty:
+            missing_edges = list(zip(missing_edges["i"], missing_edges["j"]))
+            print(f'WARNING: The following edges were not found in the static network:\n{missing_edges}')
+
+        # Only keep edges that are present in the static network
+        edges = edges[~missing_edges_indices]
+        return _class.from_edge_list_dataframe(edges, normalise, threshold, binary)
+
+    @classmethod
+    def from_static_network_and_node_list_dataframe(
+            _class,
+            static_network,
+            nodes,
+            combine_node_weights=lambda x, y: x * y,
+            normalise=False,
+            threshold=0,
+            binary=False):
+
+        # 'static_network' should be a networkx.Graph.
+
+        # 'nodes' should be a pandas.DataFrame with two or three columns, representing node, time and (optionally)
+        # weight, respectively. If the weight column is omitted then all weights are taken to be 1.
+
+        # If 'normalise' is True, all weights will be divided through by the max weight across all edges.
+
+        # For nodes i and j, if the static network contains an edge ij, and at time t the edge ij has weight r at
+        # least 'threshold' (after normalisation), then the temporal network will contain an edge ij at time t of
+        # weight r. Here r is the result of passing the weight of i and the weight of j to 'combine_node_weights'
+
+        # If 'binary' is True, then any positive edges that exceed the threshold will be set to 1.
+
+        if len(nodes.columns) == 2:
+            nodes['w'] = 1
+        elif len(nodes.columns) != 3:
+            raise ValueError('Node list must have either 2 or 3 columns.')
+        nodes.columns = ['i', 't', 'w']
+
+        missing_nodes_indices = nodes.apply(lambda node: not static_network.has_node(node['i']), axis=1)
+        missing_nodes = nodes[missing_nodes_indices]
+        if not missing_nodes.empty:
+            print(f'WARNING: The following nodes were not found in the static network:\n{missing_nodes["i"].values}')
+
+        # Only keep nodes that are present in the static network
+        nodes = nodes[~missing_nodes_indices]
+        # Merge to form temporal edge data
+        edges = nodes.merge(nodes, left_on='t', right_on='t', suffixes=('_1', '_2'))
+        # Remove duplicates and self-loops
+        edges = edges[edges['i_1'] < edges['i_2']]
+        # Add column for combined weight
+        edges['r'] = edges.apply(lambda edge: combine_node_weights(edge['w_1'], edge['w_2']), axis=1)
+        edges = edges[['i_1', 'i_2', 't', 'r']]
+
+        return _class.from_edge_list_dataframe(edges, normalise, threshold, binary)
 
 
-def load_file(filepath):
-    file_type = pathlib.Path(filepath).suffix.lower()
-    if file_type == '.npy':
-        loaded = np.load(filepath, allow_pickle=True)
-    elif file_type in ['.pkl', '.pickle']:
-        loaded = pickle.load(filepath)
-    else:
-        raise ValueError(f'Unknown file type "{file_type}" - consider loading the file yourself then '
-                         f'using a different constructor')
-    return loaded
+    @classmethod
+    def from_static_network_and_node_table_dataframe(
+            _class,
+            static_network,
+            node_table,
+            combine_node_weights=lambda x, y: x * y,
+            normalise=False,
+            threshold=0,
+            binary=False):
+
+        # 'static_network' should be a networkx.Graph.
+
+        # 'temporal_node_table' should be a pandas.DataFrame whose columns represent the nodes of the graph,
+        # and whose rows contain the temporal data for those nodes. The DataFrame should be indexed by time points,
+        # which should be numeric values. For other parameters, see 'from_static_network_and_node_list_dataframe'.
+
+        def get_node_list(node_table, node):
+            node_list = node_table[node].to_frame()
+            node_list['i'] = node
+            node_list['t'] = node_list.index
+            node_list = node_list[['i', 't', node]]
+            node_list.columns = ['i', 't', 'w']
+            return node_list
+
+        node_lists = [get_node_list(node_table, node) for node in node_table.columns]
+        node_list = pd.concat(node_lists, ignore_index=True)
+
+        return _class.from_static_network_and_node_list_dataframe(
+            static_network, node_list, combine_node_weights, normalise, threshold, binary)

44fe1a4a84672f0e87c4d9679b328ce7d6708794
OzYossarian <alex.townsend-teague@outlook.com>
2020-08-24 11:25:31 +0100

    Speeding up creation of temporal networks by removing instances where we apply a function to a dataframe row by row

--- networks/TemporalNetwork.py
+++ networks/TemporalNetwork.py
@@ -70,6 +70,8 @@ class TemporalNetwork:
         nodes = sorted(set(edges[['i', 'j']].values.flatten()))
         names_to_ids = {key: i for i, key in enumerate(nodes)}
         ids_to_names = {i: key for i, key in enumerate(nodes)}
+
+        # ToDo - this is now the slowest step in creating a TemporalNetwork; speed it up?
         edges = edges.replace(names_to_ids)
 
         edges = edges.sort_values('t')
@@ -79,7 +81,8 @@ class TemporalNetwork:
             # For compatibility with teneto, shift all times so that we start at time 0
             edges['t'] -= start_time
 
-        return _class(teneto.TemporalNetwork(from_df=edges), start_time, ids_to_names)
+        teneto_network = teneto.TemporalNetwork(from_df=edges)
+        return _class(teneto_network, start_time, ids_to_names)
 
     @classmethod
     def from_static_network_and_edge_list_dataframe(
@@ -110,16 +113,19 @@ class TemporalNetwork:
             raise ValueError('Edge list should have either 3 or 4 columns.')
         edges.columns = ['i', 'j', 't', 'w']
 
-        # ToDo: applying this lambda is VERY slow - perhaps instead convert static_network.edges into a pandas
-        # dataframe and join on it? Should be faster.
-        missing_edges_indices = edges.apply(lambda edge: not static_network.has_edge(edge['i'], edge['j']), axis=1)
-        missing_edges = edges[missing_edges_indices]
-        if not missing_edges.empty:
-            missing_edges = list(zip(missing_edges["i"], missing_edges["j"]))
+        static_network_edges = pd.DataFrame(static_network.edges)
+        static_network_edges.columns = ['static_i', 'static_j']
+        edges = edges.merge(static_network_edges, how='left', left_on=['i', 'j'], right_on=['static_i', 'static_j'])
+        edges = edges.merge(static_network_edges, how='left', left_on=['i', 'j'], right_on=['static_j', 'static_i'])
+
+        missing_edges_indices = edges['static_i_x'].isnull() & edges['static_i_y'].isnull()
+        if missing_edges_indices.any():
+            missing_edges = edges[missing_edges_indices]
+            missing_edges = list(zip(missing_edges['i'], missing_edges['j']))
             print(f'WARNING: The following edges were not found in the static network:\n{missing_edges}')
 
         # Only keep edges that are present in the static network
-        edges = edges[~missing_edges_indices]
+        edges = edges[~missing_edges_indices][['i', 'j', 't', 'w']]
         return _class.from_edge_list_dataframe(edges, normalise, threshold, binary)
 
     @classmethod
@@ -143,6 +149,9 @@ class TemporalNetwork:
         # least 'threshold' (after normalisation), then the temporal network will contain an edge ij at time t of
         # weight r. Here r is the result of passing the weight of i and the weight of j to 'combine_node_weights'
 
+        # Note that 'combine_node_weights' is applied to whole columns at a time, for efficiency. Therefore any
+        # unvectorizable lambda functions will throw an error.
+
         # If 'binary' is True, then any positive edges that exceed the threshold will be set to 1.
 
         if len(nodes.columns) == 2:
@@ -151,19 +160,23 @@ class TemporalNetwork:
             raise ValueError('Node list must have either 2 or 3 columns.')
         nodes.columns = ['i', 't', 'w']
 
-        missing_nodes_indices = nodes.apply(lambda node: not static_network.has_node(node['i']), axis=1)
-        missing_nodes = nodes[missing_nodes_indices]
-        if not missing_nodes.empty:
+        static_network_nodes = pd.DataFrame(static_network.nodes)
+        static_network_nodes.columns = ['static_i']
+        nodes = nodes.merge(static_network_nodes, how='left', left_on='i', right_on='static_i')
+
+        missing_nodes_indices = nodes['static_i'].isnull()
+        if missing_nodes_indices.any():
+            missing_nodes = nodes[missing_nodes_indices]
             print(f'WARNING: The following nodes were not found in the static network:\n{missing_nodes["i"].values}')
 
         # Only keep nodes that are present in the static network
-        nodes = nodes[~missing_nodes_indices]
+        nodes = nodes[~missing_nodes_indices][['i', 't', 'w']]
         # Merge to form temporal edge data
         edges = nodes.merge(nodes, left_on='t', right_on='t', suffixes=('_1', '_2'))
         # Remove duplicates and self-loops
         edges = edges[edges['i_1'] < edges['i_2']]
         # Add column for combined weight
-        edges['r'] = edges.apply(lambda edge: combine_node_weights(edge['w_1'], edge['w_2']), axis=1)
+        edges['r'] = combine_node_weights(edges['w_1'], edges['w_2'])
         edges = edges[['i_1', 'i_2', 't', 'r']]
 
         return _class.from_edge_list_dataframe(edges, normalise, threshold, binary)
